3b8ad36118ad93917be2d50ef857e188b9aed2ea
Vincent <booncheng89@gmail.com>
2021-03-04 21:57:48 +0800

    Convert Trip booking app from RN to expo version

--- App.js
+++ App.js
@@ -1,21 +1,127 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+    Image,
+    TouchableOpacity
+} from 'react-native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// screens
+import { Onboarding, DestinationDetail } from "./screens/";
+// extra screens
+import Tabs from "./navigation/tabs";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import { icons, COLORS, SIZES } from './constants';
+import { useFonts } from 'expo-font';
+
+const theme = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        border: "transparent",
+    },
+};
+
+const Stack = createStackNavigator();
+
+const App = () => {
+    const [loaded] = useFonts({
+        "Roboto-Black" : require('./assets/fonts/Roboto-Black.ttf'),
+        "Roboto-Bold" : require('./assets/fonts/Roboto-Bold.ttf'),
+        "Roboto-Regular" : require('./assets/fonts/Roboto-Regular.ttf'),
+
+    })
+    
+    if(!loaded){
+    return null;
+    }
+    return (
+        <NavigationContainer theme={theme}>
+            <Stack.Navigator
+                initialRouteName={'Onboarding'}
+            >
+                {/* Screens */}
+                <Stack.Screen
+                    name="Onboarding"
+                    component={Onboarding}
+                    options={{
+                        title: null,
+                        headerStyle: {
+                            backgroundColor: COLORS.white
+                        },
+                        headerLeft: null,
+                        headerRight: () => (
+                            <TouchableOpacity
+                                style={{ marginRight: SIZES.padding }}
+                                onPress={() => console.log("Pressed")}
+                            >
+                                <Image
+                                    source={icons.barMenu}
+                                    resizeMode="contain"
+                                    style={{
+                                        width: 25,
+                                        height: 25,
+                                    }}
+                                />
+                            </TouchableOpacity>
+                        ),
+                    }}
+                />
+
+                <Stack.Screen
+                    name="DestinationDetail"
+                    component={DestinationDetail}
+                    options={{ headerShown: false }}
+                />
+
+                {/* Tabs */}
+                < Stack.Screen
+                    name="Home"
+                    component={Tabs}
+                    options={{
+                        title: null,
+                        headerStyle: {
+                            backgroundColor: COLORS.white
+                        },
+                        headerLeft: ({ onPress }) => (
+                            <TouchableOpacity
+                                style={{ marginLeft: SIZES.padding }}
+                                onPress={onPress}
+                            >
+                                <Image
+                                    source={icons.back}
+                                    resizeMode="contain"
+                                    style={{
+                                        width: 25,
+                                        height: 25,
+                                    }}
+                                />
+                            </TouchableOpacity>
+                        ),
+                        headerRight: () => (
+                            <TouchableOpacity
+                                style={{ marginRight: SIZES.padding }}
+                                onPress={() => console.log("Menu")}
+                            >
+                                <Image
+                                    source={icons.menu}
+                                    resizeMode="contain"
+                                    style={{
+                                        width: 25,
+                                        height: 25,
+                                    }}
+                                />
+                            </TouchableOpacity>
+                        ),
+                    }}
+                />
+
+
+            </Stack.Navigator>
+        </NavigationContainer >
+    );
+};
+
+export default () => {
+    return <App />;
+};
